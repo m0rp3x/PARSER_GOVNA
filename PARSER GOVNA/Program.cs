9fe0f41d2360ccf1b56f75be635d5bc8c495154e
m0rp3x <yurash612@gmail.com>
2023-09-09 23:00:14 +0300

    Файнали икс2

--- PARSER GOVNA/Program.cs
+++ PARSER GOVNA/Program.cs
@@ -3,35 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
 using OfficeOpenXml;
 
 class Program
 {
-    static async System.Threading.Tasks.Task Main(string[] args)
+    static async Task Main(string[] args)
     {
-        // Create a new Excel package
-        using (var package = new ExcelPackage(@"C:\Users\koval\RiderProjects\PARSER GOVNA\PARSER GOVNA\Penis.xlsx"))
+        string excelFilePath = @"C:\Users\koval\RiderProjects\PARSER GOVNA\PARSER GOVNA\Penis.xlsx";
+        string linksFilePath = @"C:\Users\koval\RiderProjects\PARSER GOVNA\PARSER GOVNA\links.txt";
+
+        await ProcessCompaniesAsync(excelFilePath, linksFilePath);
+    }
+
+    static async Task ProcessCompaniesAsync(string excelFilePath, string linksFilePath)
+    {
+        using (var package = new ExcelPackage(new FileInfo(excelFilePath)))
         {
             var workbook = package.Workbook;
             var worksheet = workbook.Worksheets.Add("Компании");
 
-            // Add column headers
             string[] headers = { "Название", "Описание", "Адрес", "Телефон", "Почта", "Сайт" };
             worksheet.Cells["A1"].LoadFromArrays(new List<string[]>() { headers });
 
-            // Read links from the file
-            List<string> links = new List<string>();
-            using (StreamReader reader = new StreamReader(@"C:\Users\koval\RiderProjects\PARSER GOVNA\PARSER GOVNA\links.txt"))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    links.Add(line);
-                }
-            }
-
-            // Base URL for company pages
+            List<string> links = ReadLinksFromFile(linksFilePath);
             string baseUrl = "https://manufacturers.ru";
 
             using (HttpClient httpClient = new HttpClient())
@@ -45,47 +41,20 @@ class Program
                     HtmlDocument doc = new HtmlDocument();
                     doc.LoadHtml(html);
 
-                    // Extract company information
                     string company_name = doc.DocumentNode.SelectSingleNode("//h1[@class='like-h1']").InnerText;
                     string description = doc.DocumentNode.SelectSingleNode("//p").InnerText;
 
-                    // Extract contact information
                     HtmlNode contact_info = doc.DocumentNode.SelectSingleNode("//div[@id='contact-list']");
 
-                    // Extract address information using regular expression
-                    HtmlNode addressNode = doc.DocumentNode.SelectSingleNode("//td[contains(text(), 'Адрес')]/following-sibling::td/p");
-                    string address = addressNode != null ? addressNode.InnerText.Trim() : "Адрес не найден";
+                    string address = ExtractAddress(contact_info, doc);
+                    string phone = ExtractPhone(html);
+                    string email = ExtractEmail(html);
+                    string website = ExtractWebsite(html);
 
-                    string phonePattern = @"tel:([\d\s()+-]+)";
-                    MatchCollection phoneMatches = Regex.Matches(html, phonePattern);
-
-                    List<string> phones = new List<string>();
-
-                    foreach (Match match in phoneMatches)
-                    {
-                        phones.Add(match.Groups[1].Value);
-                    }
-
-                    string phone = phones.Count > 0 ? string.Join(", ", phones) : "Телефон не найден";
-
-                    string emailPattern = @"mailto:([\w\.-]+@[\w\.-]+)";
-                    Match emailMatch = Regex.Match(html, emailPattern);
-
-                    string email = emailMatch.Success ? emailMatch.Groups[1].Value : "Почта не найдена";
-                    // Extract website information using regular expression
-                    string websitePattern = @"<a href=""(https?://[^""]+)""";
-                    Match websiteMatch = Regex.Match(html, websitePattern);
-
-                    string website = websiteMatch.Success ? websiteMatch.Groups[1].Value : "Сайт не найден";
-
-
-                    // Create company info array
                     string[] company_info = { company_name, description, address, phone, email, website };
 
-                    // Add the company info to the worksheet
                     worksheet.Cells[worksheet.Dimension.End.Row + 1, 1].LoadFromArrays(new List<string[]>() { company_info });
 
-                    // Output debug information
                     Console.WriteLine($"Ссылка: {baseUrl}{link}");
                     Console.WriteLine($"Добавлена компания: {company_name}");
                     Console.WriteLine($"Адрес: {address}");
@@ -95,9 +64,58 @@ class Program
                 }
             }
 
-            // Save the Excel file
-            FileInfo excelFile = new FileInfo("Penis.xlsx");
+            FileInfo excelFile = new FileInfo(excelFilePath);
             await package.SaveAsAsync(excelFile);
         }
+    }
+
+    static List<string> ReadLinksFromFile(string filePath)
+    {
+        List<string> links = new List<string>();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                links.Add(line);
+            }
+        }
+
+        return links;
+    }
+
+    static string ExtractAddress(HtmlNode contactInfoNode, HtmlDocument doc)
+    {
+        HtmlNode addressNode = contactInfoNode.SelectSingleNode("//td[contains(text(), 'Адрес')]/following-sibling::td/p");
+        return addressNode != null ? addressNode.InnerText.Trim() : "Адрес не найден";
+    }
+
+    static string ExtractPhone(string html)
+    {
+        string phonePattern = @"tel:([\d\s()+-]+)";
+        MatchCollection phoneMatches = Regex.Matches(html, phonePattern);
+        List<string> phones = new List<string>();
+
+        foreach (Match match in phoneMatches)
+        {
+            phones.Add(match.Groups[1].Value);
+        }
+
+        return phones.Count > 0 ? string.Join(", ", phones) : "Телефон не найден";
+    }
+
+    static string ExtractEmail(string html)
+    {
+        string emailPattern = @"mailto:([\w\.-]+@[\w\.-]+)";
+        Match emailMatch = Regex.Match(html, emailPattern);
+        return emailMatch.Success ? emailMatch.Groups[1].Value : "Почта не найдена";
+    }
+
+    static string ExtractWebsite(string html)
+    {
+        string websitePattern = @"<a href=""(https?://[^""]+)""";
+        Match websiteMatch = Regex.Match(html, websitePattern);
+        return websiteMatch.Success ? websiteMatch.Groups[1].Value : "Сайт не найден";
     }
 }
